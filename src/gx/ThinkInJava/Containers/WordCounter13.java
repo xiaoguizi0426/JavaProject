63351bf68e6928768bd3b1a0009eff77b22338e1
gxbaggio <350769410@qq.com>
2015-09-16 09:50:13 +0800

    Map
    如果要使用自己的类作为HashMap的键，必须同时重载hashCode()和equals()方法。因为默认的Object的hashCode()方法生成散列码默认是使用对象的地址计算散列码，Object.equals()也是比较对象的地址。

---
+++ src/gx/ThinkInJava/Containers/WordCounter13.java
@@ -0,0 +1,32 @@
+package gx.ThinkInJava.Containers;
+
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.TreeSet;
+
+/*
+ * Containers.13
+ * 创建一个单词出现次数的计数器
+ */
+public class WordCounter13 {
+	public static void main(String[] args) {
+		// File whose words are to be counted:
+//		String fileName = "WordCounter13.java";
+//		// Set of unique words in file:
+//		Set<String> words = new TreeSet<String>(new TextFile(fileName, "\\W+"));
+//		// Create initialize array of correct length:
+//		AssociativeArray<String,Integer> wordCount =
+//			new AssociativeArray<String,Integer>(words.size());
+//		// Word list of all words in file:
+//		ArrayList<String> fileList = new TextFile(fileName, "\\W+");
+//		// Count appearances of each unique word and add to array:
+//		for(String s : words) {
+//			int count = 0;
+//			for(String t : fileList) {
+//				if(t.equals(s)) count++;
+//			}
+//			wordCount.put(s, count);
+//		}
+//		print(wordCount);
+	}
+}
